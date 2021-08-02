00e69d551adedb99ee7abe48c85fc2ff21e1c981
Michael Astalos <michaeldont@gmail.com>
2021-08-01 22:20:08 -0400

    shuffle working to show unqiue random activities, next need to add content to containers

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,8 @@ var test;
 
 var restaurantList = {};
 var eventList = {};
+var usedRandomNumbers = [];
+
 const newDateBtn = document.querySelector(".new-date-btn")
 const savedDateBtn = document.querySelector(".saved-date-btn")
 const nextBtn = document.querySelector(".next-btn")
@@ -15,6 +17,52 @@ const filterPage = document.querySelector(".filter-page")
 const criteriaPage = document.querySelector(".criteria-page")
 const newDatePage = document.querySelector(".new-date-page")
 const savedDatePage = document.querySelector(".saved-date-page")
+var restaurantCheckBox;
+//currently Movies isnt being used
+var moviesCheckBox;
+var eventsCheckBox;
+
+
+function shuffle(){
+    var randomlySelected = Math.floor(Math.random()*20);
+    var controller = true;
+    while(controller) {
+        if(usedRandomNumbers.indexOf(randomlySelected)==-1){
+            controller = false;
+            console.log(randomlySelected);
+        }
+        //NEED TO BE DISPLAYED USING MODAL
+        else if (usedRandomNumbers.length==20) {
+            window.alert("no more to loop through")
+            controller = true;
+        }
+
+        else {randomlySelected = Math.floor(Math.random()*20)}
+        
+    }
+    usedRandomNumbers.push(randomlySelected);
+    console.log(usedRandomNumbers);
+}
+
+function reshuffle() {
+    clearContent()
+    shuffle();
+}
+
+function clearContent(){
+    var content = document.querySelector('.yelp-container');
+    while(content.firstChild){
+        content.removeChild(content.firstChild);
+    }
+    var content = document.querySelector('.fandango-container');
+    while(content.firstChild){
+        content.removeChild(content.firstChild);
+    }
+    var content = document.querySelector('.ticketmaster-container');
+    while(content.firstChild){
+        content.removeChild(content.firstChild);
+    }
+}
 
 newDateBtn.onclick=()=>{
     filterPage.classList.add("filterActivate");
@@ -28,7 +76,11 @@ savedDateBtn.onclick=()=>{
 
 //Filter parameter page 
 nextBtn.onclick=()=>{
+    usedRandomNumbers = []
     filterPage.classList.remove("filterActivate");
+    restaurantCheckBox = document.getElementById("Restuarants").checked;
+    moviesCheckBox = document.getElementById("Movies").checked;
+    eventsCheckBox = document.getElementById("Events").checked;
     criteriaPage.classList.add("criteriaActivate");
 }
 
@@ -37,7 +89,7 @@ createBtn.onclick=()=>{
     var zipcode = $('#zipcode').val();
     
     fetch(
-        "https://api.documenu.com/v2/restaurants/zip_code/"+zipcode+"?size=30&key=a33fecb2f255c04e008c528cf89286a2"
+        "https://api.documenu.com/v2/restaurants/zip_code/"+zipcode+"?size=20&key=a33fecb2f255c04e008c528cf89286a2"
         )
         .then(function(response1) {
           return response1.json();
